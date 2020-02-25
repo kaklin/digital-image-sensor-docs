377926d7ddc870c6c28c14dfd255deece8ebc231
evcka <filip.kaklin@gmail.com>
2020-02-25 19:41:59 +0000

    add theme settings

--- source/conf.py
+++ source/conf.py
@@ -53,6 +53,17 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    # 'canonical_url': '',
+    'style_external_links': True,
+    # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

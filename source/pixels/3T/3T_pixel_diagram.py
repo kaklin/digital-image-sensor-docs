16c5ae1709afa8511d0b257f1b8d1090bedb916b
kaklin <filip.kaklin@gmail.com>
2024-02-04 09:33:29 +0000

    Add timing diagram and description to 3T pixel

--- source/pixels/3T/3T_pixel_diagram.py
+++ source/pixels/3T/3T_pixel_diagram.py
@@ -1,5 +1,6 @@
 import schemdraw
 from schemdraw import elements as elm
+schemdraw.config(bgcolor='#FCFCFC')
 
 with schemdraw.Drawing() as d:
     d.config(unit=2)
@@ -13,7 +14,7 @@ with schemdraw.Drawing() as d:
 
     sn = elm.Line().right(0.75).at(pd.end).label('SN', ofst=(0.2,0.1))
 
-    QSF = elm.AnalogNFet(offset_gate=False, arrow=False).theta(180).anchor('gate').at(sn.end).label('MSF', loc='right')
+    QSF = elm.AnalogNFet(offset_gate=False, arrow=False).theta(180).anchor('gate').at(sn.end).label('SF', loc='right')
     elm.Line().up(1.25)
     elm.Vdd().label('VSF')
 
@@ -28,6 +29,10 @@ with schemdraw.Drawing() as d:
     out = elm.Line().up(4.5).label('Shared\nColumn ', loc='right')
     elm.SourceI().down().at(down.end).label('Current sink\noutside array')
     elm.Ground()
+
+    elm.Dot().at(down.end)
+    elm.Line().right(1.75)
+    elm.Tag().right().label('ADC')
 
 
 
